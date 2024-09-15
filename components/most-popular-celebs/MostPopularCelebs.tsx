22f6a46ea1dab6f91ca522f8e9a57be6d83fe337
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2024-09-15 18:41:02 +0300

    Fixing the Taiwind merge { cn } issue

--- components/most-popular-celebs/MostPopularCelebs.tsx
+++ components/most-popular-celebs/MostPopularCelebs.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import CelebCard from './CelebCard'
-import CelebCarousel from './CelebCarousel'
+import React from "react";
+import CelebCard from "./CelebCard";
+import CelebCarousel from "./CelebCarousel";
 import { Celebrity, PopularCelebs } from "@/_types/types";
-import { PopularMovieStars } from "@/_lib/popular_movie_stars";
+import { PopularMovieStars } from "@/lib/popular_movie_stars";
 
 function fetchCelebs(): PopularCelebs["results"] {
   // Slice the first 10 celebrities from the results array
   return PopularMovieStars.results.slice(0, 10);
 }
 
-
 export default function MostPopularCelebs() {
   const celebs = fetchCelebs();
 
   return (
     <section className="py-12">
-      <h2 className="text-3xl font-bold text-center mb-8">Most Popular Celebrities</h2>
+      <h2 className="text-3xl font-bold text-center mb-8">
+        Most Popular Celebrities
+      </h2>
       <CelebCarousel celebs={celebs} />
     </section>
   );
